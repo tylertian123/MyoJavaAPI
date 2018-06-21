d0631719c768af47eff5e08e4f319b95ebe3b00f
Tyler Tian <tylertian123@gmail.com>
2018-06-20 22:14:52 -0400

    Add Pose class

---
+++ Java/src/com/thalmic/myo/Pose.java
@@ -0,0 +1,27 @@
+package com.thalmic.myo;
+
+public enum Pose {
+	unknown, rest, fist, fingersSpread, waveIn, waveOut, doubleTap;
+	
+	@Override
+	public String toString() {
+		switch(this) {
+		case unknown:
+			return "unknown";
+		case rest:
+			return "rest";
+		case fist:
+			return "fist";
+		case fingersSpread:
+			return "fingersSpread";
+		case waveIn:
+			return "waveIn";
+		case waveOut:
+			return "waveOut";
+		case doubleTap:
+			return "doubleTap";
+		default:
+			return "unknown";
+		}
+	}
+}
