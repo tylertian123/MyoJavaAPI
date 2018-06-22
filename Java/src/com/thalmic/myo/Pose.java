65574a1fd87e246f6d96f7bf92f3549fafd1bfd8
Tyler Tian <tylertian123@gmail.com>
2018-06-22 16:51:29 -0400

    Add documentation for Pose enum

--- Java/src/com/thalmic/myo/Pose.java
+++ Java/src/com/thalmic/myo/Pose.java
@@ -1,13 +1,53 @@
 package com.thalmic.myo;
 
+/**
+ * A pose represents a detected configuration of the user's hand.
+ *
+ */
 public enum Pose {
 	
-	unknown, rest, fist, fingersSpread, waveIn, waveOut, doubleTap;
+	/**
+	 * Unknown pose.
+	 */
+	unknown, 
+	/**
+	 * The rest pose.
+	 */
+	rest, 
+	/**
+	 * The fist pose.
+	 */
+	fist, 
+	/**
+	 * The spread fingers pose.
+	 */
+	fingersSpread,
+	/**
+	 * The waving in pose.
+	 */
+	waveIn, 
+	/**
+	 * The waving out pose.
+	 */
+	waveOut, 
+	/**
+	 * The double tap (middle finger to thumb) pose.
+	 */
+	doubleTap;
 	
+	/**
+	 * Returns the type of this pose.
+	 * @deprecated This method is only here because it is in the C++ Myo API. As {@link Pose} is a enum itself, instead of
+	 * a class containing an enum, this method simply does "{@code return this;}".
+	 * @return The type of this pose (the pose itself)
+	 */
 	public Pose type() {
 		return this;
 	}
 	
+	/**
+	 * Returns a human-readable string representation of the pose.
+	 */
 	@Override
 	public String toString() {
 		switch(this) {
