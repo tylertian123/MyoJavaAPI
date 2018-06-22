477c98de64bc6e80d74db64e2224844b39f04338
Tyler Tian <tylertian123@gmail.com>
2018-06-22 00:36:11 -0400

    Create additional constructors for exceptions

--- Java/src/com/thalmic/myo/JNIException.java
+++ Java/src/com/thalmic/myo/JNIException.java
@@ -5,12 +5,26 @@ public class JNIException extends RuntimeException {
 	/**
 	 * 
 	 */
-	private static final long serialVersionUID = -4985976880244896038L;
+	private static final long serialVersionUID = 8544388924714627158L;
 
 	public JNIException() {
 		super();
 	}
+
 	public JNIException(String message) {
 		super(message);
 	}
+
+	public JNIException(Throwable cause) {
+		super(cause);
+	}
+
+	public JNIException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public JNIException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+	}
+
 }
--- Java/src/com/thalmic/myo/MyoException.java
+++ Java/src/com/thalmic/myo/MyoException.java
@@ -5,12 +5,26 @@ public class MyoException extends RuntimeException {
 	/**
 	 * 
 	 */
-	private static final long serialVersionUID = -5118395062252676619L;
-	
+	private static final long serialVersionUID = 4201749969607436643L;
+
 	public MyoException() {
 		super();
 	}
+
 	public MyoException(String message) {
 		super(message);
 	}
+
+	public MyoException(Throwable cause) {
+		super(cause);
+	}
+
+	public MyoException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public MyoException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+	}
+
 }
