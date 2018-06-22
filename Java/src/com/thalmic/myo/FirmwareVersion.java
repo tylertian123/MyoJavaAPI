0164937f870ed85549850501838adcfa8941b844
Tyler Tian <tylertian123@gmail.com>
2018-06-22 13:27:47 -0400

    Add documentation for FirmwareVersion

--- Java/src/com/thalmic/myo/FirmwareVersion.java
+++ Java/src/com/thalmic/myo/FirmwareVersion.java
@@ -5,6 +5,24 @@ package com.thalmic.myo;
  *
  */
 public class FirmwareVersion {
-	
-	public int firmwareVersionMajor, firmwareVersionMinor, firmwareVersionPatch, firmwareVersionHardwareRev;
+	/**
+	 * {@link Myo}'s major version must match the required major version.
+	 * <p>Note that this field is unsigned.</p>
+	 */
+	public int firmwareVersionMajor;
+	/**
+	 * {@link Myo}'s minor version must match the required minor version.
+	 * <p>Note that this field is unsigned.</p>
+	 */
+	public int firmwareVersionMinor;
+	/**
+	 * {@link Myo}'s patch version must be greater or equal to the required patch version.
+	 * <p>Note that this field is unsigned.</p>
+	 */
+	public int firmwareVersionPatch;
+	/**
+	 * {@link Myo}'s hardware revision; not used to detect firmware version mismatch.
+	 * <p>Note that this field is unsigned.</p>
+	 */
+	public int firmwareVersionHardwareRev;
 }
